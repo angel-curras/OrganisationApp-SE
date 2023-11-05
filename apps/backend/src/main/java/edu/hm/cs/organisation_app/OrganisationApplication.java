73770735815a3f22ddae12baacae2b82b0a9a319
Angel Curras Sanchez <angel.curras@hm.edu>
2023-11-05 10:15:53 +0100

    Initial backend app

---
+++ apps/backend/src/main/java/edu/hm/cs/organisation_app/OrganisationApplication.java
@@ -0,0 +1,13 @@
+package edu.hm.cs.organisation_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganisationApplication.class, args);
+	}
+
+}
---
+++ apps/backend/src/test/java/edu/hm/cs/organisation_app/OrganisationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hm.cs.organisation_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganisationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
