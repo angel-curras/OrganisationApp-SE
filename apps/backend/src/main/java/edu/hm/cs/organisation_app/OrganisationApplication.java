0f36cc04dc9cbfbb04e481d20437738b6ff26767
Angel Curras Sanchez <angel.curras@hm.edu>
2023-12-26 13:13:23 +0100

    Added test for OrganisationApplication

--- apps/backend/src/main/java/edu/hm/cs/organisation_app/OrganisationApplication.java
+++ apps/backend/src/main/java/edu/hm/cs/organisation_app/OrganisationApplication.java
@@ -3,11 +3,21 @@ package edu.hm.cs.organisation_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Represents a OrganisationApplication.
+ *
+ * @author Angel Curras Sanchez, Leon Beckert, Jan Weller
+ */
 @SpringBootApplication
 public class OrganisationApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OrganisationApplication.class, args);
-    }
+  /**
+   * Main method.
+   *
+   * @param args arguments
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(OrganisationApplication.class, args);
+  } // end of main()
 
-}
+} // end of class OrganisationApplication
