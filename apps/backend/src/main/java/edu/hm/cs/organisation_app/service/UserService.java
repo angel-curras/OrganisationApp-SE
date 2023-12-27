e430d8bd3cb376ae08985c5fee69b78e349a42a0
Angel Curras Sanchez <angel.curras@hm.edu>
2023-12-27 15:10:12 +0100

    Removed usernames functionality because its redundant

--- apps/backend/src/main/java/edu/hm/cs/organisation_app/service/UserService.java
+++ apps/backend/src/main/java/edu/hm/cs/organisation_app/service/UserService.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-
 import java.util.List;
 
 /**
@@ -41,9 +40,6 @@ public class UserService {
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found."));
   }
 
-  public List<String> getAllUsernames() {
-    return repository.findAll().stream().map(AppUser::getUserName).collect(java.util.stream.Collectors.toList());
-  }
   public AppUser createUser(AppUser user) {
     return repository.save(user);
   }
