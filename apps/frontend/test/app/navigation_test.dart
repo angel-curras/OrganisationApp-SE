91c2fceeeff742769878afabafaa65afc6d5599b
Angel Curras Sanchez <angel.curras@hm.edu>
2023-12-29 14:38:20 +0100

    Web Pages tested in the navigation test

--- apps/frontend/test/app/navigation_test.dart
+++ apps/frontend/test/app/navigation_test.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:organisation_app/app/routes.dart';
 import 'package:organisation_app/main.dart';
+import 'package:organisation_app/pages/chatgpt/chatgpt_page.dart';
 import 'package:organisation_app/pages/home/home_page.dart';
+import 'package:organisation_app/pages/moodle/moodle_page.dart';
+import 'package:organisation_app/pages/primuss/primuss_page.dart';
 import 'package:organisation_app/pages/todos/todos_page.dart';
 import 'package:organisation_app/settings/app_settings.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import '../shared/mock_web_view_plattform.dart';
 
 class MainTestAppNavigation extends StatelessWidget {
   final SharedPreferences _preferences;
@@ -35,6 +41,7 @@ class MainTestAppNavigation extends StatelessWidget {
 void main() {
   setUp(() async {
     await setUpEnvironment();
+    WebViewPlatform.instance = MockWebViewPlatform();
   }); // end of setUp()
 
   testWidgets('Test: Navigation', (tester) async {
@@ -57,6 +64,27 @@ void main() {
     await tester.tap(find.byKey(const Key('todosTile')));
     await tester.pumpAndSettle();
     expect(find.byType(TodosPage), findsOneWidget);
+
+    // Navigate to Moodle
+    await tester.tap(find.byIcon(Icons.menu));
+    await tester.pumpAndSettle();
+    await tester.tap(find.byKey(const Key('moodleTile')));
+    await tester.pumpAndSettle();
+    expect(find.byType(MoodlePage), findsOneWidget);
+
+    // Navigate to Primuss
+    await tester.tap(find.byIcon(Icons.menu));
+    await tester.pumpAndSettle();
+    await tester.tap(find.byKey(const Key('primussTile')));
+    await tester.pumpAndSettle();
+    expect(find.byType(PrimussPage), findsOneWidget);
+
+    // Navigate to ChatGpt
+    await tester.tap(find.byIcon(Icons.menu));
+    await tester.pumpAndSettle();
+    await tester.tap(find.byKey(const Key('chatgptTile')));
+    await tester.pumpAndSettle();
+    expect(find.byType(ChatGptPage), findsOneWidget);
 
     // Navigate to Login
     await tester.tap(find.byIcon(Icons.menu));
