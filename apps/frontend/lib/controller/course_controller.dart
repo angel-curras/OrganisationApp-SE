24c58bfeff04f3bcb9b1b90e1c789826e05b1224
Angel Curras Sanchez <angel.curras@hm.edu>
2023-12-30 20:07:57 +0100

    dialog to update a course added

--- apps/frontend/lib/controller/course_controller.dart
+++ apps/frontend/lib/controller/course_controller.dart
@@ -11,11 +11,13 @@ import 'package:universal_html/html.dart';
 class CourseController {
   static final Logger _logger = Logger();
   static final _apiUrl = Environment.apiUrl;
-  final http.Client _httpClient;
+  final http.Client _client;
   static const int _timeout = 3;
 
-  CourseController({http.Client? client})
-      : _httpClient = client ?? http.Client();
+  CourseController({http.Client? client}) : _client = client ?? http.Client();
+
+  // Getters
+  http.Client get client => _client;
 
   Future<List<Course>> getAllCoursesForUser(String username) async {
     // Check response from backend.
@@ -26,7 +28,7 @@ class CourseController {
 
     final http.Response response;
     try {
-      response = await _httpClient
+      response = await _client
           .get(Uri.parse(requestUrl))
           .timeout(const Duration(seconds: _timeout));
     } on TimeoutException catch (timeoutException) {
@@ -67,7 +69,7 @@ class CourseController {
     );
 
     String requestUrl = '$_apiUrl/courses';
-    final response = await _httpClient
+    final response = await _client
         .post(
       Uri.parse(requestUrl),
       headers: <String, String>{
@@ -85,7 +87,7 @@ class CourseController {
       case HttpStatus.created:
         _logger
             .i("HTTP Status 201: Created. User '$userName' enrolled in course "
-                "'$courseId'.");
+            "'$courseId'.");
         break;
       case HttpStatus.conflict:
         _logger.e("HTTP Status 409: Conflict. User '$userName' already "
@@ -98,4 +100,32 @@ class CourseController {
 
     return statusCode;
   } // end of enroll()
+
+  Future<bool> updateCourse(Course course) async {
+    _logger.i(
+        "Updating course '${course.name}' with course id '${course.id}'.");
+    http.Response response = await _client
+        .put(Uri.parse('$_apiUrl/courses/${course.id}'),
+        headers: <String, String>{'Content-Type': 'application/json'},
+        body: course.toJsonString())
+        .timeout(const Duration(seconds: _timeout), onTimeout: () {
+      return http.Response('Error: timeout', HttpStatus.requestTimeout);
+    });
+
+    return response.statusCode != HttpStatus.ok;
+  }
+
+  Future<bool> deleteCourse(Course course) async {
+    _logger
+        .i("Deleting course '${course.name}' with course id '${course.id}'.");
+
+    http.Response response = await _client
+        .delete(Uri.parse('$_apiUrl/courses/${course.id}'))
+        .timeout(const Duration(seconds: _timeout), onTimeout: () {
+      return http.Response('Error: timeout', HttpStatus.requestTimeout);
+    });
+
+    // check response from backend
+    return response.statusCode != HttpStatus.ok;
+  }
 } // end of class CourseController
---
+++ apps/frontend/lib/pages/my_courses/update_course_dialog.dart
@@ -0,0 +1,469 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:organisation_app/controller/course_controller.dart';
+import 'package:organisation_app/model/course.dart';
+import 'package:organisation_app/model/weekday.dart';
+
+class UpdateCourseDialog extends StatefulWidget {
+  // Fields.
+  final CourseController _courseController;
+  final Course _course;
+
+  UpdateCourseDialog(
+      {super.key, required http.Client client, required Course course})
+      : _course = course,
+        _courseController = CourseController(client: client);
+
+  @override
+  State<UpdateCourseDialog> createState() => _UpdateCourseDialogState();
+}
+
+class _UpdateCourseDialogState extends State<UpdateCourseDialog> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Future<void> _onUpdateClicked(BuildContext context) async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    await widget._courseController.updateCourse(widget._course);
+
+    if (!context.mounted) {
+      return;
+    }
+    Navigator.of(context).pop();
+  }
+
+  Future<TimeOfDay?> _selectTime(BuildContext context) async {
+    final TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    return pickedTime;
+  }
+
+  Future<DateTime?> _selectDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime(2030),
+    );
+
+    return pickedDate;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(children: [
+            // Start of the list view.
+
+            // Course card infos:
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Course name:
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          const Expanded(
+                            child: Text(
+                              "Course: ",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Text(
+                              widget._course.name,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    // Course responsible:
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          const Expanded(
+                            child: Text(
+                              "Responsible: ",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Text(
+                              widget._course.responsible,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    // Course progress:
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          const Expanded(
+                            child: Text(
+                              "Progress: ",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Text(
+                              "${widget._course.progress}%",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          const Expanded(
+                            child: Text(
+                              "Start date: ",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                DateTime? pickedDate =
+                                    await _selectDate(context);
+                                if (pickedDate != null) {
+                                  setState(() {
+                                    widget._course.startDate = pickedDate;
+                                  });
+                                }
+                              },
+                              child: Text(widget._course.startDate == null
+                                  ? "Select date"
+                                  : '${widget._course.startDate!.day}/${widget._course.startDate!.month}/${widget._course.startDate!.year}'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          const Expanded(
+                            child: Text(
+                              "End date: ",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                DateTime? pickedDate =
+                                    await _selectDate(context);
+                                if (pickedDate != null) {
+                                  setState(() {
+                                    widget._course.endDate = pickedDate;
+                                  });
+                                }
+                              },
+                              child: Text(widget._course.endDate == null
+                                  ? "Select date"
+                                  : '${widget._course.endDate!.day}/${widget._course.endDate!.month}/${widget._course.endDate!.year}'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            // Course settings: lecture
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Lecture:',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          const Expanded(
+                            child: Text(
+                              "Day of week: ",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Expanded(
+                              flex: 2,
+                              child: DropdownButtonFormField<Weekday>(
+                                key: const Key("lecture_week_day"),
+                                value: widget._course.lectureWeekday,
+                                decoration: const InputDecoration(
+                                  hintText: "Please select the lecture day",
+                                ),
+                                onChanged: (Weekday? newValue) {
+                                  if (newValue == null) {
+                                    return;
+                                  }
+                                  setState(() {
+                                    widget._course.lectureWeekday = newValue;
+                                  });
+                                },
+                                items: Weekday.values.map((weekday) {
+                                  return DropdownMenuItem<Weekday>(
+                                      value: weekday,
+                                      child: Text(
+                                        weekday.toJSON(),
+                                      ));
+                                }).toList(),
+                              )),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          const Expanded(
+                            child: Text(
+                              "Start time: ",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                TimeOfDay? pickedTime =
+                                    await _selectTime(context);
+                                if (pickedTime != null) {
+                                  setState(() {
+                                    widget._course.lectureStartTime =
+                                        pickedTime;
+                                  });
+                                }
+                              },
+                              child: Text(
+                                widget._course.lectureStartTime == null
+                                    ? "Select time"
+                                    : widget._course.lectureStartTime!
+                                        .format(context),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          const Expanded(
+                            child: Text(
+                              "End time: ",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                TimeOfDay? pickedTime =
+                                    await _selectTime(context);
+                                if (pickedTime != null) {
+                                  setState(() {
+                                    widget._course.lectureEndTime = pickedTime;
+                                  });
+                                }
+                              },
+                              child: Text(
+                                widget._course.lectureEndTime == null
+                                    ? "Select time"
+                                    : widget._course.lectureEndTime!
+                                        .format(context),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            // Course settings: lab
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Lab:',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          const Expanded(
+                            child: Text(
+                              "Day of week: ",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Expanded(
+                              flex: 2,
+                              child: DropdownButtonFormField<Weekday>(
+                                key: const Key("lab_week_day"),
+                                value: widget._course.labWeekday,
+                                decoration: const InputDecoration(
+                                  hintText: "Please select the lab day",
+                                ),
+                                onChanged: (Weekday? newValue) {
+                                  if (newValue == null) {
+                                    return;
+                                  }
+                                  setState(() {
+                                    widget._course.labWeekday = newValue;
+                                  });
+                                },
+                                items: Weekday.values.map((weekday) {
+                                  return DropdownMenuItem<Weekday>(
+                                      value: weekday,
+                                      child: Text(
+                                        weekday.toJSON(),
+                                      ));
+                                }).toList(),
+                              )),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          const Expanded(
+                            child: Text(
+                              "Start time: ",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                TimeOfDay? pickedTime =
+                                    await _selectTime(context);
+                                if (pickedTime != null) {
+                                  setState(() {
+                                    widget._course.labStartTime = pickedTime;
+                                  });
+                                }
+                              },
+                              child: Text(
+                                widget._course.labStartTime == null
+                                    ? "Select time"
+                                    : widget._course.labStartTime!
+                                        .format(context),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          const Expanded(
+                            child: Text(
+                              "End time: ",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                TimeOfDay? pickedTime =
+                                    await _selectTime(context);
+                                if (pickedTime != null) {
+                                  setState(() {
+                                    widget._course.labEndTime = pickedTime;
+                                  });
+                                }
+                              },
+                              child: Text(
+                                widget._course.labEndTime == null
+                                    ? "Select time"
+                                    : widget._course.labEndTime!
+                                        .format(context),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            // Save button:
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () => _onUpdateClicked(context),
+                child: const Text("Update"),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
