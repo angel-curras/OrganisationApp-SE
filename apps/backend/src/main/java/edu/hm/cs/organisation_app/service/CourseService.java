a8fd4305d154104a4fa28838341aef0e4582a65e
Angel Curras Sanchez <angel.curras@hm.edu>
2024-01-13 09:51:54 +0100

    Test documentation for unit test course controller in backend added

--- apps/backend/src/main/java/edu/hm/cs/organisation_app/service/CourseService.java
+++ apps/backend/src/main/java/edu/hm/cs/organisation_app/service/CourseService.java
@@ -29,6 +29,14 @@ public class CourseService {
 
 
   /* Constructors */
+
+  /**
+   * Creates a new CourseService.
+   *
+   * @param courseRepository The course repository.
+   * @param moduleRepository The module repository.
+   * @param userRepository   The user repository.
+   */
   @Autowired
   public CourseService(CourseRepository courseRepository, ModuleRepository moduleRepository,
                        UserRepository userRepository) {
@@ -38,11 +46,57 @@ public class CourseService {
   }
 
   /* Methods */
-  @Transactional
-  public List<Course> getAllCourses() {
-    return courseRepository.findAll();
-  }
+  /* GET */
 
+  /**
+   * Gets all courses for a user.
+   *
+   * @param userName The username.
+   * @return A list of courses.
+   */
+  @Transactional
+  public List<Course> getAllCoursesForUser(String userName) {
+
+    // Check if the user is authorized to create a course.
+    AppUser user =
+            userRepository.findById(userName)
+                    .orElseThrow(
+                            () -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found."));
+
+    // Check if the user is already subscribed to this course.
+    return user.getCourses();
+  } // end of getAllCoursesForUser()
+
+  /**
+   * Gets a course by its id.
+   *
+   * @param courseId The course id.
+   * @return The course.
+   */
+  @Transactional
+  public Course getCourseById(Long courseId) {
+    return courseRepository.findById(courseId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Course not found."));
+  } // end of getCourseById()
+
+  /**
+   * Gets all tasks for a course.
+   *
+   * @param courseId The course id.
+   * @return A list of tasks.
+   */
+  @Transactional
+  public List<Task> getAllTasksForCourse(Long courseId) {
+    return courseRepository.findById(courseId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Course not found.")).getTasks();
+  } // end of getAllTasksForCourse()
+
+  /* POST */
+
+  /**
+   * Creates a course.
+   *
+   * @param courseSubscription The course subscription.
+   * @return The created course.
+   */
   @Transactional
   public Course createCourse(CourseSubscription courseSubscription) {
 
@@ -68,25 +122,25 @@ public class CourseService {
     for (Course course : userCourses) {
       if (Objects.equals(course.getModuleId(), moduleId)) {
         throw new ResponseStatusException(HttpStatus.CONFLICT, "User already subscribed to this course.");
-      }
-    }
+      } // end of if
+    } // end of for
 
     // Create the course and save it to the database
     Course course = new Course(module, user);
     user.addCourse(course);
     return courseRepository.save(course);
-  }
 
-  @Transactional
-  public Course getCourseById(Long courseId) {
-    return courseRepository.findById(courseId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Course not found."));
-  }
+  } // end of createCourse()
 
-  @Transactional
-  public void deleteCourseById(Long courseId) {
-    courseRepository.deleteById(courseId);
-  }
+  /* PUT */
 
+  /**
+   * Updates a course by its id.
+   *
+   * @param courseId  The course id.
+   * @param newCourse The new course.
+   * @return The updated course.
+   */
   @Transactional
   public Course updateCourseById(Long courseId, Course newCourse) {
     Course course = courseRepository.findById(courseId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Course not found."));
@@ -104,30 +158,26 @@ public class CourseService {
 
     // Save the course to the database.
     return courseRepository.save(course);
-  }
+  } // end of updateCourseById()
 
+  /* DELETE */
 
+  /**
+   * Deletes a course by its id.
+   *
+   * @param courseId The course id.
+   */
   @Transactional
-  public List<Course> getAllCoursesForUser(String userName) {
+  public void deleteCourseById(Long courseId) {
+    courseRepository.deleteById(courseId);
+  } // end of deleteCourseById()
 
-    // Check if the user is authorized to create a course.
-    AppUser user =
-            userRepository.findById(userName)
-                    .orElseThrow(
-                            () -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found."));
-
-    // Check if the user is already subscribed to this course.
-    return user.getCourses();
-  }
-
-  @Transactional
-  public List<Task> getAllTasksForCourse(Long courseId) {
-    return courseRepository.findById(courseId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Course not found.")).getTasks();
-  }
-
+  /**
+   * Deletes all courses.
+   */
   @Transactional
   public void deleteAll() {
     courseRepository.deleteAll();
-  }
+  } // end of deleteAll()
 
 } // end of class CourseService
